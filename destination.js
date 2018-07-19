c7446fedaf57e649cca8094eae7579c8eae71e16
chelsea13 <41412964+chelsea13@users.noreply.github.com>
2018-07-19 13:40:38 +0800

    Create destination.js

---
+++ destination.js
@@ -0,0 +1,63 @@
+{
+    "id": 145,
+    "name": "Halo halo de iloko",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place1.jpg",
+    "priceCurrency": "₱",
+    "price": 120,
+    "lat": 16.6167,
+    "lng": 120.3197
+  },
+  {
+    "id": 148,
+    "name": "Pugad Resort",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place2.jpg",
+    "priceCurrency": "₱",
+    "price": 200,
+    "lat": 16.2833,
+    "lng": 120.4833
+  },
+  {
+    "id": 201,
+    "name": "Bahay na Bato",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place3.jpg",
+    "priceCurrency": "₱",
+    "price": 50,
+    "lat": 16.8472,
+    "lng": 120.3517
+  },
+  {
+    "id": 205,
+    "name": "Marand Resort",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place4.jpg",
+    "priceCurrency": "₱",
+    "price": 150,
+    "lat": 16.5264,
+    "lng": 120.3294
+  },
+  {
+    "id": 210,
+    "name": "Splash Wave Resort",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place5.jpg",
+    "priceCurrency": "PHP",
+    "price": 150,
+    "lat": 16.3188,
+    "lng": 120.4687
+  },
+  {
+    "id": 211,
+    "name": "Kahuna Resort",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place6.jpg",
+    "priceCurrency": "₱",
+    "price": 200,
+    "lat": 16.6583,
+    "lng": 120.3216
+  },
+    {
+    "id": 211,
+    "name": "House",
+    "imageUrl": "https://raw.githubusercontent.com/Chegalinato/map/places/images/place7.jpg",
+    "priceCurrency": "₱",
+    "price": 0,
+    "lat": 16.6167,
+    "lng": 120.3167
+  }
